3b2ac078c02e0450f90cee0960de8da54d039f34
waramity <waramity.re@gmail.com>
2023-10-02 06:06:38 +0700

    add delete every post in uninstall

--- uninstall.php
+++ uninstall.php
@@ -5,3 +5,15 @@
  * 
  * @package BasicPlugin
  */
+
+ if(!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+ }
+
+
+// Clear Database stored data
+ $books = get_posts(array('post_type' => 'book', 'numberposts' => -1));
+
+ foreach($books as $book) {
+    wp_delete_post($book->ID, true);
+ }
