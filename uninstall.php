76a5f76b328618468e2e653124794b8553320069
waramity <waramity.re@gmail.com>
2023-10-02 06:31:23 +0700

    iAdd uninstall functionality to clean up 'book' post type data

--- uninstall.php
+++ uninstall.php
@@ -17,3 +17,8 @@
  foreach($books as $book) {
     wp_delete_post($book->ID, true);
  }
+
+ // Access the database via SQL
+ global $wpdb;
+ $wpdb->query("DELETE FROM wp_posts WHERE post_type='book'");
+ $wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
