97686cb295df090eb17a113f60b49c93b229e18d
unknown <waramity.re@gmail.com>
2023-10-02 16:30:12 +0700

    (3) Admin Custom Fields

--- templates/admin.php
+++ templates/admin.php
@@ -1,1 +1,12 @@
-<h1>Basic Plugin</h1>
+<div class="wrap">
+	<h1>Basic Plugin</h1>
+	<?php settings_errors(); ?>
+
+	<form method="post" action="options.php">
+		<?php 
+			settings_fields( 'basic_options_group' );
+			do_settings_sections( 'basic_plugin_menu_slug' );
+			submit_button();
+		?>
+	</form>
+</div>
