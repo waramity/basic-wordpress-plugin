7431863c4ad187fb6c0fb41e1b407840e6cbc752
unknown <waramity.re@gmail.com>
2023-10-14 15:18:30 +0700

    clean code

---
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package  BasicPlugin
+ */
+namespace Inc\Api\Callbacks;
+
+
+class CptCallbacks
+{
+    public function cptSection()
+    {
+        echo 'Manage your Custom Post Types';
+    }
+
+    public function checkboxField($args)
+    {
+        $name = $args['label_for'];
+        $classes = $args['class'];
+        $option_name = $args['option_name'];
+        $checkbox = get_option($option_name);
+        $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+        echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+    }
+}
--- inc/Pages/Dashboard.php
+++ inc/Pages/Dashboard.php
@@ -19,7 +19,6 @@ class Dashboard extends BaseController
 	public $callbacks;
 	public $callbacks_mngr;
 	public $pages = array();
-	// public $subpages = array();
 
 	public function register()
 	{
@@ -28,22 +27,15 @@ class Dashboard extends BaseController
 		$this->callbacks = new AdminCallbacks();
 		$this->callbacks_mngr = new ManagerCallbacks();
 
-		// add_action('admin_menu', array($this, 'add_admin_pages'));
-		// $this->settings->addPages( $this->pages )->register();
 		$this->setPages();
-		// $this->setSubpages();
 
 		$this->setSettings();
 		$this->setSections();
 		$this->setFields();
 
-		// $this->settings->addPages($this->pages)->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
 		$this->settings->addPages($this->pages)->withSubPage('Dashboard')->register();
 	}
 
-	// public function add_admin_pages() {
-	// 	add_menu_page( 'Basic Plugin Page Title', 'Basic Plugin Menu Title', 'manage_options', 'basic_plugin_menu_slug', array( $this, 'admin_index' ), 'dashicons-store', 110 );
-	// }
 	public function setPages()
 	{
 		$this->pages = array(
@@ -59,103 +51,13 @@ class Dashboard extends BaseController
 		);
 	}
 
-	// public function setSubpages()
-	// {
-	// 	$this->subpages = array(
-	// 		array(
-	// 			'parent_slug' => 'basic_plugin_menu_slug',
-	// 			'page_title' => 'Custom Post Types',
-	// 			'menu_title' => 'CPT',
-	// 			'capability' => 'manage_options',
-	// 			'menu_slug' => 'basic_cpt',
-	// 			'callback' => array($this->callbacks, 'adminCpt')
-	// 		),
-	// 		array(
-	// 			'parent_slug' => 'basic_plugin_menu_slug',
-	// 			'page_title' => 'Custom Taxonomies',
-	// 			'menu_title' => 'Taxonomies',
-	// 			'capability' => 'manage_options',
-	// 			'menu_slug' => 'basic_taxonomies',
-	// 			'callback' => array($this->callbacks, 'adminTaxonomy')
-	// 		),
-	// 		array(
-	// 			'parent_slug' => 'basic_plugin_menu_slug',
-	// 			'page_title' => 'Custom Widgets',
-	// 			'menu_title' => 'Widgets',
-	// 			'capability' => 'manage_options',
-	// 			'menu_slug' => 'basic_widgets',
-	// 			'callback' => array($this->callbacks, 'adminWidget')
-	// 		)
-	// 	);
-	// }
-
 	public function setSettings()
 	{
-		// $args = array();
-		// var_dump($this->managers);
-		// var_dump($this->plugin_path);
-
-		// foreach ($this->managers as $key => $value) {
-		// 	$args[] = array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => $key,
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	);
-		// }
-
 		$args[] = array(
 			'option_group' => 'basic_plugin_settings',
 			'option_name' => 'basic_plugin',
 			'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
 		);
-
-		// $args = array(
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'cpt_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'taxonomy_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'media_widget',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'gallery_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'testimonial_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'templates_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'login_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'membership_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	),
-		// 	array(
-		// 		'option_group' => 'basic_plugin_settings',
-		// 		'option_name' => 'chat_manager',
-		// 		'callback' => array($this->callbacks_mngr, 'checkboxSanitize')
-		// 	)
-		// );
 
 		$this->settings->setSettings($args);
 	}
@@ -166,7 +68,6 @@ class Dashboard extends BaseController
 			array(
 				'id' => 'basic_admin_index',
 				'title' => 'Settings Managers',
-				// 'callback' => array($this->callbacks, 'basicAdminSection'),
 				'callback' => array($this->callbacks_mngr, 'adminSectionManager'),
 				'page' => 'basic_plugin_menu_slug'
 			)
@@ -195,15 +96,6 @@ class Dashboard extends BaseController
 		}
 
 		$this->settings->setFields($args);
-	}
-
-	public function admin_index()
-	{
-		// require_once PLUGIN_PATH . 'templates/admin.php';
-		// echo $this->plugin_path;
-		require_once $this->plugin_path . 'templates/admin.php';
-
-		// require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
 	}
 
 }
