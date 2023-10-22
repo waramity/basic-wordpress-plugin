2f2317087b1b890b9d23843ed21cf33307b4e16c
unknown <waramity.re@gmail.com>
2023-10-22 19:32:30 +0700

    finish store arrays in wp_options

--- inc/Api/Callbacks/CptCallbacks.php
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -1,40 +1,48 @@
-<?php
+<?php 
 /**
  * @package  BasicPlugin
  */
 namespace Inc\Api\Callbacks;
 
-
 class CptCallbacks
 {
-    public function cptSectionManager()
-    {
-        echo 'Create as many Custom Post Types as you want.';
-    }
-    public function cptsanitize($input)
-    {
-        return $input;
 
-    }
+	public function cptSectionManager()
+	{
+		echo 'Create as many Custom Post Types as you want.';
+	}
 
-    public function textField($args)
-    {
-        $name = $args['label_for'];
-        $option_name = $args['option_name'];
-        $input = get_option($option_name);
-        $value = $input[$name];
+	public function cptSanitize( $input )
+	{
+		$output = get_option('basic_plugin_cpt');
 
-        echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $args['placeholder'] . '">';
-    }
+		foreach ($output as $key => $value) {
+			if ($input['post_type'] === $key) {
+				$output[$key] = $input;
+			} else {
+				$output[$input['post_type']] = $input;
+			}
+		}
+		
+		return $output;
+	}
 
-    public function checkboxField($args)
-    {
-        $name = $args['label_for'];
-        $classes = $args['class'];
-        $option_name = $args['option_name'];
-        $checkbox = get_option($option_name);
-        $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+	public function textField( $args )
+	{
+		$name = $args['label_for'];
+		$option_name = $args['option_name'];
+		$input = get_option( $option_name );
 
-        echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
-    }
+		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
+	}
+
+	public function checkboxField( $args )
+	{
+		$name = $args['label_for'];
+		$classes = $args['class'];
+		$option_name = $args['option_name'];
+		$checkbox = get_option( $option_name );
+
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class=""><label for="' . $name . '"><div></div></label></div>';
+	}
 }
--- inc/Base/CustomPostTypeController.php
+++ inc/Base/CustomPostTypeController.php
@@ -15,15 +15,17 @@ use Inc\Api\Callbacks\AdminCallbacks;
 class CustomPostTypeController extends BaseController
 {
 	public $settings;
+
 	public $callbacks;
+
 	public $cpt_callbacks;
+
 	public $subpages = array();
+
 	public $custom_post_types = array();
 
 	public function register()
 	{
-		// if ( ! $this->activated( 'cpt_manager' ) ) return;
-
 		if (!$this->activated('cpt_manager'))
 			return;
 
@@ -34,8 +36,11 @@ class CustomPostTypeController extends BaseController
 		$this->cpt_callbacks = new CptCallbacks();
 
 		$this->setSubpages();
+
 		$this->setSettings();
+
 		$this->setSections();
+
 		$this->setFields();
 
 		$this->settings->addSubPages($this->subpages)->register();
@@ -45,8 +50,8 @@ class CustomPostTypeController extends BaseController
 		if (!empty($this->custom_post_types)) {
 			add_action('init', array($this, 'registerCustomPostTypes'));
 		}
-
 	}
+
 	public function setSubpages()
 	{
 		$this->subpages = array(
@@ -60,7 +65,6 @@ class CustomPostTypeController extends BaseController
 			)
 		);
 	}
-
 
 	public function setSettings()
 	{
@@ -80,7 +84,7 @@ class CustomPostTypeController extends BaseController
 		$args = array(
 			array(
 				'id' => 'basic_cpt_index',
-				'title' => 'Settings Managers',
+				'title' => 'Custom Post Type Manager',
 				'callback' => array($this->cpt_callbacks, 'cptSectionManager'),
 				'page' => 'basic_cpt'
 			)
@@ -101,7 +105,8 @@ class CustomPostTypeController extends BaseController
 				'args' => array(
 					'option_name' => 'basic_plugin_cpt',
 					'label_for' => 'post_type',
-					'placeholder' => 'eg. product'
+					'placeholder' => 'eg. product',
+					'array' => 'post_type'
 				)
 			),
 			array(
@@ -113,7 +118,8 @@ class CustomPostTypeController extends BaseController
 				'args' => array(
 					'option_name' => 'basic_plugin_cpt',
 					'label_for' => 'singular_name',
-					'placeholder' => 'eg. Product'
+					'placeholder' => 'eg. Product',
+					'array' => 'post_type'
 				)
 			),
 			array(
@@ -125,7 +131,8 @@ class CustomPostTypeController extends BaseController
 				'args' => array(
 					'option_name' => 'basic_plugin_cpt',
 					'label_for' => 'plural_name',
-					'placeholder' => 'eg. Products'
+					'placeholder' => 'eg. Products',
+					'array' => 'post_type'
 				)
 			),
 			array(
@@ -137,7 +144,8 @@ class CustomPostTypeController extends BaseController
 				'args' => array(
 					'option_name' => 'basic_plugin_cpt',
 					'label_for' => 'public',
-					'class' => 'ui-toggle'
+					'class' => 'ui-toggle',
+					'array' => 'post_type'
 				)
 			),
 			array(
@@ -149,7 +157,8 @@ class CustomPostTypeController extends BaseController
 				'args' => array(
 					'option_name' => 'basic_plugin_cpt',
 					'label_for' => 'has_archive',
-					'class' => 'ui-toggle'
+					'class' => 'ui-toggle',
+					'array' => 'post_type'
 				)
 			)
 		);
@@ -161,74 +170,63 @@ class CustomPostTypeController extends BaseController
 	{
 		$options = get_option('basic_plugin_cpt');
 
-		// foreach ($options as $option) {
+		if ($options) {
 
-			$this->custom_post_types[] = array(
-				'post_type'             => $options['post_type'],
-				'name'                  => $options['plural_name'],
-				'singular_name'         => $options['singular_name'],
-				'menu_name'             => $options['plural_name'],
-				'name_admin_bar'        => $options['singular_name'],
-				'archives'              => $options['singular_name'] . ' Archives',
-				'attributes'            => $options['singular_name'] . ' Attributes',
-				'parent_item_colon'     => 'Parent ' . $options['singular_name'],
-				'all_items'             => 'All ' . $options['plural_name'],
-				'add_new_item'          => 'Add New ' . $options['singular_name'],
-				'add_new'               => 'Add New',
-				'new_item'              => 'New ' . $options['singular_name'],
-				'edit_item'             => 'Edit ' . $options['singular_name'],
-				'update_item'           => 'Update ' . $options['singular_name'],
-				'view_item'             => 'View ' . $options['singular_name'],
-				'view_items'            => 'View ' . $options['plural_name'],
-				'search_items'          => 'Search ' . $options['plural_name'],
-				'not_found'             => 'No ' . $options['singular_name'] . ' Found',
-				'not_found_in_trash'    => 'No ' . $options['singular_name'] . ' Found in Trash',
-				'featured_image'        => 'Featured Image',
-				'set_featured_image'    => 'Set Featured Image',
-				'remove_featured_image' => 'Remove Featured Image',
-				'use_featured_image'    => 'Use Featured Image',
-				'insert_into_item'      => 'Insert into ' . $options['singular_name'],
-				'uploaded_to_this_item' => 'Upload to this ' . $options['singular_name'],
-				'items_list'            => $options['plural_name'] . ' List',
-				'items_list_navigation' => $options['plural_name'] . ' List Navigation',
-				'filter_items_list'     => 'Filter' . $options['plural_name'] . ' List',
-				'label'                 => $options['singular_name'],
-				'description'           => $options['plural_name'] . 'Custom Post Type',
-				'supports'              => array( 'title', 'editor', 'thumbnail' ),
-				'taxonomies'            => array( 'category', 'post_tag' ),
-				'hierarchical'          => false,
-				'public'                => $options['public'],
-				'show_ui'               => true,
-				'show_in_menu'          => true,
-				'menu_position'         => 5,
-				'show_in_admin_bar'     => true,
-				'show_in_nav_menus'     => true,
-				'can_export'            => true,
-				'has_archive'           => $options['has_archive'],
-				'exclude_from_search'   => false,
-				'publicly_queryable'    => true,
-				'capability_type'       => 'post'
-			);
+			foreach ($options as $option) {
 
-		// }
+				$this->custom_post_types[] = array(
+					'post_type' => $option['post_type'],
+					'name' => $option['plural_name'],
+					'singular_name' => $option['singular_name'],
+					'menu_name' => $option['plural_name'],
+					'name_admin_bar' => $option['singular_name'],
+					'archives' => $option['singular_name'] . ' Archives',
+					'attributes' => $option['singular_name'] . ' Attributes',
+					'parent_item_colon' => 'Parent ' . $option['singular_name'],
+					'all_items' => 'All ' . $option['plural_name'],
+					'add_new_item' => 'Add New ' . $option['singular_name'],
+					'add_new' => 'Add New',
+					'new_item' => 'New ' . $option['singular_name'],
+					'edit_item' => 'Edit ' . $option['singular_name'],
+					'update_item' => 'Update ' . $option['singular_name'],
+					'view_item' => 'View ' . $option['singular_name'],
+					'view_items' => 'View ' . $option['plural_name'],
+					'search_items' => 'Search ' . $option['plural_name'],
+					'not_found' => 'No ' . $option['singular_name'] . ' Found',
+					'not_found_in_trash' => 'No ' . $option['singular_name'] . ' Found in Trash',
+					'featured_image' => 'Featured Image',
+					'set_featured_image' => 'Set Featured Image',
+					'remove_featured_image' => 'Remove Featured Image',
+					'use_featured_image' => 'Use Featured Image',
+					'insert_into_item' => 'Insert into ' . $option['singular_name'],
+					'uploaded_to_this_item' => 'Upload to this ' . $option['singular_name'],
+					'items_list' => $option['plural_name'] . ' List',
+					'items_list_navigation' => $option['plural_name'] . ' List Navigation',
+					'filter_items_list' => 'Filter' . $option['plural_name'] . ' List',
+					'label' => $option['singular_name'],
+					'description' => $option['plural_name'] . 'Custom Post Type',
+					'supports' => array('title', 'editor', 'thumbnail'),
+					'taxonomies' => array('category', 'post_tag'),
+					'hierarchical' => false,
+					'public' => $option['public'],
+					'show_ui' => true,
+					'show_in_menu' => true,
+					'menu_position' => 5,
+					'show_in_admin_bar' => true,
+					'show_in_nav_menus' => true,
+					'can_export' => true,
+					'has_archive' => $option['has_archive'],
+					'exclude_from_search' => false,
+					'publicly_queryable' => true,
+					'capability_type' => 'post'
+				);
+			}
+
+		}
 	}
 
 	public function registerCustomPostTypes()
 	{
-
-		// foreach ($this->custom_post_types as $post_type) {
-		// 	register_post_type(
-		// 		$post_type['post_type'],
-		// 		array(
-		// 			'labels' => array(
-		// 				'name' => $post_type['name'],
-		// 				'singular_name' => $post_type['singular_name']
-		// 			),
-		// 			'public' => $post_type['public'],
-		// 			'has_archive' => $post_type['has_archive'],
-		// 		)
-		// 	);
-		// }
 		foreach ($this->custom_post_types as $post_type) {
 			register_post_type(
 				$post_type['post_type'],
@@ -282,7 +280,4 @@ class CustomPostTypeController extends BaseController
 			);
 		}
 	}
-
-
-
 }
